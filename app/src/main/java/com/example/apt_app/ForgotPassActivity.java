1fb957eaa82c4f5f509f43a4ab1ebb5560d3ad59
kylewu2540 <kylewu@asu.edu>
2023-02-07 19:19:10 -0800

    add more stuff

--- app/src/main/java/com/example/apt_app/ForgotPassActivity.java
+++ app/src/main/java/com/example/apt_app/ForgotPassActivity.java
@@ -8,9 +8,14 @@ import androidx.appcompat.app.AlertDialog;
 
 import android.os.Bundle;
 
+import javax.mail.*;
+import java.util.*;
+import javax.mail.internet.*;
+
 public class ForgotPassActivity extends AppCompatActivity {
 
     private EditText mEmail;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,51 @@ public class ForgotPassActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forgot_pass);
 
         mEmail = findViewById(R.id.etEmail);
+        mButton = findViewById(R.id.btnSubmit);
+
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        String email = mEmail.getText().toString();
+                        final String username = "your_email@gmail.com";
+                        final String password = "your_email_password";
+
+                        Properties prop = new Properties();
+                        prop.put("mail.smtp.host", "smtp.gmail.com");
+                        prop.put("mail.smtp.port", "587");
+                        prop.put("mail.smtp.auth", "true");
+                        prop.put("mail.smtp.starttls.enable", "true");
+
+                        Session session = Session.getInstance(prop,
+                                new javax.mail.Authenticator() {
+                                    protected PasswordAuthentication getPasswordAuthentication() {
+                                        return new PasswordAuthentication(username, password);
+                                    }
+                                });
+
+                        try {
+                            Message message = new MimeMessage(session);
+                            message.setFrom(new InternetAddress("from@gmail.com"));
+                            message.setRecipients(
+                                    Message.RecipientType.TO,
+                                    InternetAddress.parse("to@gmail.com")
+                            );
+                            message.setSubject("Testing Gmail SSL");
+                            message.setText("Dear Mail Crawler,"
+                                    + "\n\n Please do not spam my email!");
+
+                            Transport.send(message);
+                            System.out.println("Done");
+                        } catch (MessagingException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/apt_app/HomeActivity.java
+++ app/src/main/java/com/example/apt_app/HomeActivity.java
@@ -3,6 +3,9 @@ package com.example.apt_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Create a list of items to be displayed in the table view
+        String[] items = {"Games", "Book room", "Laundry", "Payment Options (utilities/rent)", "Community", "Maintenance", "Rooftop Bar"};
+
+        // Connect the ListView in the layout to a Java object
+        ListView listView = findViewById(R.id.list_view);
+
+        // Create an ArrayAdapter to populate the ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+
+        // Set the adapter to the ListView
+        listView.setAdapter(adapter);
+
+        // Set an OnItemClickListener on the ListView
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the item that was clicked
+                String item = (String) parent.getItemAtPosition(position);
+
+                // Start the DetailActivity, passing the item as an extra
+                /*Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("item", item);
+                startActivity(intent);*/
+            }
+        });
     }
 }
